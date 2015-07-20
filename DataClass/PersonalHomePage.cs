047274774aec869d142a716dc161c1134debe428
bme319 <zjubme319@163.com>
2015-07-20 14:56:45 +0800

    20150720 池胜强
    20150716-20150718 北京 海军总医院 出差回来后的最新工程

---
+++ DataClass/PersonalHomePage.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService.DataClass
+{
+    public class PatientBasicInfo0
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Birthday { get; set; }
+        public string Gender { get; set; }
+        public string IDNo { get; set; }
+        public string Module { get; set; }
+    }
+
+    public class DoctorDetailInfo0
+    {
+        public string UserId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string HomeAddress { get; set; }
+        public string Occupation { get; set; }
+        public string Nationality { get; set; }
+        public string EmergencyContact { get; set; }
+        public string EmergencyContactPhoneNumber { get; set; }
+        public string PhotoAddress { get; set; }
+        public string Module { get; set; }
+        public string IDNo { get; set; }
+    }
+
+    public class PatientDetailInfo0
+    {
+        public string UserId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string HomeAddress { get; set; }
+        public string Occupation { get; set; }
+        public string Nationality { get; set; }
+        public string EmergencyContact { get; set; }
+        public string EmergencyContactPhoneNumber { get; set; }
+        public string PhotoAddress { get; set; }
+        public string Module { get; set; }
+        public string IDNo { get; set; }
+    }
+
+    public class PatientDetailInfo1
+    {
+        public string UserId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string HomeAddress { get; set; }
+        public string Occupation { get; set; }
+        public string Nationality { get; set; }
+        public string EmergencyContact { get; set; }
+        public string EmergencyContactPhoneNumber { get; set; }
+        public string PhotoAddress { get; set; }
+        public string Module { get; set; }
+        public string IDNo { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+    }
+
+    public class PatientALLBasicInfo
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public int Birthday { get; set; }
+        public string Gender { get; set; }
+        public string BloodType { get; set; }
+        public string IDNo { get; set; }
+        public string DoctorId { get; set; }
+        public string InsuranceType { get; set; }
+        public int InvalidFlag { get; set; }
+        public string Module { get; set; }
+    }
+
+    public class DoctorDetailInfo1
+    {
+        public string IDNo { get; set; }
+        public string PhotoAddress { get; set; }
+        public string UnitName { get; set; }
+        public string JobTitle { get; set; }
+        public string Level { get; set; }
+        public string Dept { get; set; }
+    }
+}
---
+++ DataMethod/CmMstUserDetail.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InterSystems.Data.CacheClient;
+using System.Data;
+using WebService.CommonLibrary;
+
+namespace WebService.DataMethod
+{
+    public class CmMstUserDetail
+    {
+        public static int SetData(DataConnection pclsCache, string UserId, int StartDate, string Type, string Name, string Value, string piUserId, string piTerminalName, string piTerminalIP, int piDeviceType)
+        {
+            int ret = 0;
+            try
+            {
+                if (!pclsCache.Connect())
+                {
+                    return ret;
+                }
+
+                ret = (int)Cm.MstUserDetail.SetData(pclsCache.CacheConnectionObject, UserId, StartDate, Type, Name, Value, piUserId, piTerminalName, piTerminalIP, piDeviceType);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                HygeiaComUtility.WriteClientLog(HygeiaEnum.LogType.ErrorLog, "CmMstUserDetail.SetData", "数据库操作异常！ error information : " + ex.Message + Environment.NewLine + ex.StackTrace);
+                return ret;
+            }
+            finally
+            {
+                pclsCache.DisConnect();
+            }
+        }
+
+        // GetIDByInput LS 2015-03-26  TDY 20150507修改
+        public static string GetIDByInput(DataConnection pclsCache, string Type, string Name)
+        {
+            string ret = "";
+            try
+            {
+                if (!pclsCache.Connect())
+                {
+                    return ret;
+                }
+
+                ret = Cm.MstUserDetail.GetIDByInput(pclsCache.CacheConnectionObject, Type, Name);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                HygeiaComUtility.WriteClientLog(HygeiaEnum.LogType.ErrorLog, "Cm.MstUserDetail.GetIDByInput", "数据库操作异常！ error information : " + ex.Message + Environment.NewLine + ex.StackTrace);
+                return ret;
+            }
+            finally
+            {
+                pclsCache.DisConnect();
+            }
+        }
+
+        // CheckRepeat LS 2015-03-26  TDY 20150507修改
+        public static int CheckRepeat(DataConnection pclsCache, string Input, string Type)
+        {
+            int ret = 0;
+            try
+            {
+                if (!pclsCache.Connect())
+                {
+                    return ret;
+                }
+
+                ret = (int)Cm.MstUserDetail.CheckRepeat(pclsCache.CacheConnectionObject, Input, Type);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                HygeiaComUtility.WriteClientLog(HygeiaEnum.LogType.ErrorLog, "Cm.MstUserDetail.CheckRepeat", "数据库操作异常！ error information : " + ex.Message + Environment.NewLine + ex.StackTrace);
+                return ret;
+            }
+            finally
+            {
+                pclsCache.DisConnect();
+            }
+        }
+
+        //CheckPassworkByInput LS 2015-03-26  TDY 20150507修改
+        public static int CheckPasswordByInput(DataConnection pclsCache, string Type, string Name, string Password)
+        {
+            int ret = 0;
+            try
+            {
+                if (!pclsCache.Connect())
+                {
+                    return ret;
+                }
+
+                ret = (int)Cm.MstUserDetail.CheckPasswordByInput(pclsCache.CacheConnectionObject, Type, Name, Password);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                HygeiaComUtility.WriteClientLog(HygeiaEnum.LogType.ErrorLog, "Cm.MstUserDetail.CheckPasswordByInput", "数据库操作异常！ error information : " + ex.Message + Environment.NewLine + ex.StackTrace);
+                return ret;
+            }
+            finally
+            {
+                pclsCache.DisConnect();
+            }
+        }
+
+        //CheckPhoneNumber TDY 2015-04-13  TDY 20150507修改
+        public static int CheckPhoneNumber(DataConnection pclsCache, string Type, string piName, string UserId)
+        {
+            int ret = 0;
+            try
+            {
+                if (!pclsCache.Connect())
+                {
+                    return ret;
+                }
+
+                ret = (int)Cm.MstUserDetail.CheckPhoneNumber(pclsCache.CacheConnectionObject, Type, piName, UserId);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                HygeiaComUtility.WriteClientLog(HygeiaEnum.LogType.ErrorLog, "Cm.MstUserDetail.CheckPhoneNumber", "数据库操作异常！ error information : " + ex.Message + Environment.NewLine + ex.StackTrace);
+                return ret;
+            }
+            finally
+            {
+                pclsCache.DisConnect();
+            }
+        }
+
+        //获取Token  GL 2015-04-24
+        public static string GetTokenInCache(DataConnection pclsCache, string UserId)
+        {
+            string ret = "";
+            try
+            {
+                if (!pclsCache.Connect())
+                {
+                    return ret;
+                }
+
+                ret = Cm.MstUserDetail.GetTokenInCache(pclsCache.CacheConnectionObject, UserId);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                HygeiaComUtility.WriteClientLog(HygeiaEnum.LogType.ErrorLog, "Cm.MstUserDetail.GetTokenInCache", "数据库操作异常！ error information : " + ex.Message + Environment.NewLine + ex.StackTrace);
+                return ret;
+            }
+            finally
+            {
+                pclsCache.DisConnect();
+            }
+        }
+
+        // 写入Token GL 2015-04-24
+        public static int SetToken(DataConnection pclsCache, string Token, string UserId, string piUserId, string piTerminalName, string piTerminalIP, int piDeviceType)
+        {
+            int ret = 0;
+            try
+            {
+                if (!pclsCache.Connect())
+                {
+                    return ret;
+                }
+
+                ret = (int)Cm.MstUserDetail.SetToken(pclsCache.CacheConnectionObject, Token, UserId, piUserId, piTerminalName, piTerminalIP, piDeviceType);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                HygeiaComUtility.WriteClientLog(HygeiaEnum.LogType.ErrorLog, "Cm.MstUserDetail.SetToken", "数据库操作异常！ error information : " + ex.Message + Environment.NewLine + ex.StackTrace);
+                return ret;
+            }
+            finally
+            {
+                pclsCache.DisConnect();
+            }
+        }
+
+        //绑定手机号码 TDY 2015-05-27
+        public static int SetPhoneNo(DataConnection pclsCache, string UserId, string Type, string Name, string piUserId, string piTerminalName, string piTerminalIP, int piDeviceType)
+        {
+            int ret = 0;
+            try
+            {
+                if (!pclsCache.Connect())
+                {
+                    return ret;
+                }
+
+                ret = (int)Cm.MstUserDetail.SetPhoneNo(pclsCache.CacheConnectionObject, UserId, Type, Name, piUserId, piTerminalName, piTerminalIP, piDeviceType);
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                HygeiaComUtility.WriteClientLog(HygeiaEnum.LogType.ErrorLog, "Cm.MstUserDetail.SetPhoneNo", "数据库操作异常！ error information : " + ex.Message + Environment.NewLine + ex.StackTrace);
+                return ret;
+            }
+            finally
+            {
+                pclsCache.DisConnect();
+            }
+        }
+    }
+}
