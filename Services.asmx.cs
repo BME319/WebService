679dd0d8b060ac6073c22afc845691cbb79cf89c
luyao2211 <luyao2211@126.com>
2015-11-25 22:31:55 +0800

    在WebService中增加了短信验证码的发送和验证 	LY	2015-11-25

--- Services.asmx.cs
+++ Services.asmx.cs
@@ -14,6 +14,8 @@ using WebService.DataClass;
 using System.IO;
 using System.Net;
 using RongYunClassLib;
+using ServiceStack.Redis;
+using System.Security.Cryptography;
 
 namespace WebService
 {
@@ -13493,6 +13495,123 @@ namespace WebService
 
         #endregion
 
+        [WebMethod(Description = "发送短信验证码，Author：LY 2015-11-25")]
+        public string sendSMS(string mobile, string smsType, string content)//content的内容示例： 王五，2015年11月11日，海军总医院（目标姓名，预约时间，预约地点）
+        {
+            try
+            {
+                string UserId = CmMstUserDetail.GetIDByInput(_cnCache, "PhoneNo", mobile);
+                if (UserId == null)
+                    return "系统中不存在该手机号码";
+                var Client = new RedisClient("127.0.0.1", 6379);
+                var token = "849407bfab0cf4c1a998d3d6088d957b";
+                var appId = "0593b3c52f7d4f8aa9f9055585407e16";
+                var accountSid = "b839794e66174938828d1b8ea9c58412";
+                var tplId = "";
+                var param = "";
+                var Jsonstring1 = "templateSMS";
+                var Jsonstring2 = "appId";
+                var Jsonstring3 = "param";
+                var Jsonstring4 = "templateId";
+                var Jsonstring5 = "to";
+                var J6 = "{";
+                var flag = false;
+
+                Random rand = new Random();
+                var randNum = rand.Next(100000, 1000000);
+                if (smsType == "verification")
+                {
+                    tplId = "14452";
+                    param = randNum + "," + 3;
+                }
+                if (smsType == "confirmtoPatient")
+                {
+                    tplId = "16420";
+                    param = content;
+                    flag = true;
+                }
+                if (smsType == "confirmtoHealthCoach")
+                {
+                    tplId = "16419";
+                    param = content;
+                    flag = true;
+                }
+
+                string JSONData = J6 + '"' + Jsonstring1 + '"' + ':' + '{' + '"' + Jsonstring2 + '"' + ':' + '"' + appId + '"' + ',' + '"' + Jsonstring3 + '"' + ':' + '"' + param + '"' + ',' + '"' + Jsonstring4 + '"' + ':' + '"' + tplId + '"' + ',' + '"' + Jsonstring5 + '"' + ':' + '"' + mobile + '"' + '}' + '}';
+
+                if (mobile != "" && smsType != "")
+                {
+                    var Flag1 = Client.Get<string>(mobile + smsType);
+                    if (Flag1 == null || flag == true)
+                    {
+                        Client.Set<int>(mobile + smsType, randNum);
+                        Client.Expire(mobile + smsType, 3 * 60);
+                        var timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+                        MD5 MD5 = MD5.Create();
+                        var md5 = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(accountSid + token + timestamp, "MD5").ToUpper();
+
+                        System.Text.Encoding encode = System.Text.Encoding.ASCII;
+                        byte[] bytedata = encode.GetBytes(accountSid + ":" + timestamp);
+                        var authorization = Convert.ToBase64String(bytedata, 0, bytedata.Length);
+                        var length1 = md5.Length;
+                        var length2 = authorization.Length;
+
+                        string Url = "https://api.ucpaas.com/2014-06-30/Accounts/" + accountSid + "/Messages/templateSMS?sig=" + md5;
+                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                        request.Method = "POST";
+                        request.Accept = "application/json";
+                        request.ContentType = "application/json;charset=utf-8";
+                        request.Headers.Set("Authorization", authorization);
+                        //request.ContentLength = 256;
+                        //request.Headers.Set("content-type", "application/json;charset=utf-8");
+                        byte[] bytes = Encoding.UTF8.GetBytes(JSONData);
+                        request.ContentLength = bytes.Length;
+                        request.Timeout = 10000;
+                        Stream reqstream = request.GetRequestStream();
+                        reqstream.Write(bytes, 0, bytes.Length);
+                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                        Stream streamReceive = response.GetResponseStream();
+                        Encoding encoding = Encoding.UTF8;
+                        StreamReader streamReader = new StreamReader(streamReceive, encoding);
+                        string strResult = streamReader.ReadToEnd();
+                        streamReceive.Dispose();
+                        streamReader.Dispose();
+
+                        return strResult;
+
+                    }
+                    else
+                    {
+                        var time = Client.Ttl(mobile + smsType);
+                        string codeexist = "您的邀请码已发送，请等待" + time + "后重新获取";
+                        return codeexist;
+                    }
+                }
+                return null;
+            }
+            catch (WebException ex)
+            {
+                using (WebResponse response = ex.Response)
+                {
+                    HttpWebResponse httpResponse = (HttpWebResponse)response;
+                    Console.WriteLine("Error code: {0}", httpResponse.StatusCode);
+                    using (Stream data = response.GetResponseStream())
+                    using (var reader = new StreamReader(data))
+                    {
+                        string text = reader.ReadToEnd();
+                        Console.WriteLine(text);
+                    }
+                }
+                return ex.Message;
+            }
+        }
+
+        [WebMethod(Description = "验证短信验证码，Author：LY 2015-11-25")]
+        public int checkverification(string mobile, string smsType, string verification)
+        {
+            return CmMstUserDetail.checkverification(mobile, smsType, verification);
+        }
     }
 
 }
